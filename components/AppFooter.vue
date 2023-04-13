da51aed3c07c9811cb8ac359d411385e94944da0
kimminh061020 <82656330+kimminh061020@users.noreply.github.com>
2023-04-13 07:32:34 +0700

    Update AppFooter.vue
    Delete widget here

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,7 +5,6 @@
       &copy; Mark Nguyen's Website 2021
     </p>
   </div>
-  <script>(function(d){var s = d.createElement("script");s.setAttribute("data-account", "Y1wLGDw71E");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)</script>
 </footer>
 </template>
 
