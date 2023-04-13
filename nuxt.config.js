294d0f5a392128de380e33f63e0695c90b6a5b39
kimminh061020 <82656330+kimminh061020@users.noreply.github.com>
2023-04-13 07:33:55 +0700

    Update nuxt.config.js
    add widget

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    script: [
+      { src: "https://cdn.userway.org/widget.js", "data-account": "M61lrwQRlB", defer: "true" }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
